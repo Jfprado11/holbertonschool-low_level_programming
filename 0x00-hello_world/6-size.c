cdb136ff035921405a439989e8a1ea63632bb0a0
Felipe <jfpc11@misena.edu.co>
2021-02-04 03:09:24 +0000

    working with sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %ld bytes(s)\n", sizeof(char));
+	printf("ssize of a char: %ld bytes(s)\n", sizeof(char));
 	printf("size of an int: %ld bytes(s)\n", sizeof(int));
 	printf("size of a long int: %ld bytes(s)\n", sizeof(long int));
 	printf("size of a long long int: %ld bytes(s)\n", sizeof(long long int));
-	printf("size of a float: %ld bytes(s)\n", sizeof(float);
+	printf("size of a float: %ld bytes(s)\n", sizeof(float));
 	return (0);
 }
