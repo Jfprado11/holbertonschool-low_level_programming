d951b02d740576cc3e6bff853901fa3359f42b8e
Felipe <jfpc11@misena.edu.co>
2021-02-09 15:28:34 +0000

    10 times the alphaber

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int x;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}	
+	_putchar('\n');
+	}
+}
