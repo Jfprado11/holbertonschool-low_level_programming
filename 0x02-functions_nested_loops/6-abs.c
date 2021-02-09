bc43515d0e1983cdc4d0e1a700e97f04cd704458
Felipe <jfpc11@misena.edu.co>
2021-02-09 17:20:14 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_abs - gives the absulute value of a number
+ *@x: the number to be check
+ *
+ * Return: the value of an absolute
+ */
+int _abs(int x)
+{
+
+	if (x < 0)
+	{
+		x = (-1) * x;
+	}
+	return (x);
+}
