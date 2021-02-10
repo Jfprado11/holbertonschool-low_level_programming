783c382390ad2af2fd3a6a28e37f320fd30dfa85
Felipe <jfpc11@misena.edu.co>
2021-02-10 02:26:15 +0000

    print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - prints the number until 98
+ *@n: given first number
+ *
+ * Return: the sum of two numbers
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			printf("%d ,", n);
+		}
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d ,", n);
+		}
+	}
+	printf("98\n");
+}
