7a3b587285ba48e2e60bfd721c62d96ed5a2d8ef
Felipe <jfpc11@misena.edu.co>
2021-02-10 01:54:37 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *add - gives the ours from 00:00 to 23:59
+ *@a: given first number
+ *@b: given second number
+ *
+ * Return: the sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
