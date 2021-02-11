150629ee3f69485afe521c9689fde5e534f98ec6
Felipe <jfpc11@misena.edu.co>
2021-02-11 16:39:50 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *mul - Tells if the characters are uppercase
+ *@a: first varible to multiple
+ *@b: second varible to multiple
+ *
+ *Return: varbles a times b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
