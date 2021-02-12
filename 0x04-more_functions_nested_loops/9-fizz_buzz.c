d5e1b5492adcde2dc671f4b9798accaf2aa04438
Felipe <jfpc11@misena.edu.co>
2021-02-12 01:32:16 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - fizz bizz table
+ *
+ *Return: 0 success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
