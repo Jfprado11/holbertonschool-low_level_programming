48bfd52f9d94b66bdc7ab301fb5520cdf913d5d2
Felipe <jfpc11@misena.edu.co>
2021-02-17 04:02:49 +0000

    half puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *puts_half - a function to reverse a string
+ *@str: varible to make a reverse string
+ *
+ *Return: nothing
+ */
+void puts_half(char *str)
+{
+	int a, l, hlo;
+
+	l = 0;
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 0)
+	{
+		hlo = l / 2;
+	}
+	else
+	{
+		hlo = (l -1) / 2;
+	}
+	for (a = hlo; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
