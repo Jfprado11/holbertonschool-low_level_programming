23dd402d2fcebe2b702b466544507e47f3423ad0
Felipe <jfpc11@misena.edu.co>
2021-02-18 23:01:50 +0000

    concataneta

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_strcat - a function to concanate two strings
+ *@dest: variable to destinetion
+ *@src: variable sorce
+ *
+ *Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lon = 0;
+	int lon2 = 0;
+
+	while (dest[lon] != '\0')
+	{
+		lon++;
+	}
+	while (src[lon2] != '\0')
+	{
+		src[lon2] = dest[lon];
+		lon++;
+		lon2++;
+	}
+	*dest = '\0';
+	return (dest);
+}
