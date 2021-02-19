8924b3a385282210e0fe4ac3b4831226f0bd517a
Felipe <jfpc11@misena.edu.co>
2021-02-19 00:05:50 +0000

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@ char *_strcat(char *dest, char *src)
 	int lon = 0;
 	int lon2 = 0;
 
-	while (dest[lon] != '\0')
+	while (src[lon] != '\0')
 	{
 		lon++;
 	}
-	while (src[lon2] != '\0')
+	while (dest[lon2] != '\0')
 	{
-		dest[lon + 1] = src[lon2];
+		src[lon]= dest[lon2];
 		lon++;
 		lon2++;
 	}
-	dest[lon] = '\0';
-	return (dest);
+	src[lon] = '\0';
+	return (src);
 }
