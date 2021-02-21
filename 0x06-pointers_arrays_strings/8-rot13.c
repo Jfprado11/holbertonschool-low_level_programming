12f92cd71018663a563345b23f3edbe2af05140f
Felipe <jfpc11@misena.edu.co>
2021-02-21 01:58:10 +0000

    rot 13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *rot13 - make a string into a rot 13
+ *@m: varible to the array
+ *
+ *Return: encoded string
+ */
+char *rot13(char *m)
+{
+	int a;
+
+	for (a = 0; m[a] != '\0'; a++)
+	{
+		if ((m[a] >= 'a' && m[a] <= 'm') || (m[a] >= 'A' && m[a] <= 'M'))
+		{
+			m[a] = m[a] + 13;
+		}
+		else if ((m[a] >= 'n' && m[a] <= 'z') || (m[a] >= 'N' && m[a] <= 'Z'))
+		{
+			m[a] = m[a] - 13;
+		}
+	}
+	return (m);
+}
