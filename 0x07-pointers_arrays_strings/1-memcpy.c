b07587553a67364e941b88ebabbd9989b2dfe40e
Felipe <jfpc11@misena.edu.co>
2021-02-22 16:38:50 +0000

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,20 @@
-hello
+#include "holberton.h"
+
+/**
+ *_memcpy - fill memory with a constat byte
+ *@dest: destinition of the data memory to be copied
+ *@src: source of the memory
+ *@n: copies the first bytes
+ *
+ *Return: a pointer with the value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a =0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
