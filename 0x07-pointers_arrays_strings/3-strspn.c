629c869b9976ebc3ae84f59e3663e479e65e729e
Felipe <jfpc11@misena.edu.co>
2021-02-22 21:57:52 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,10 +11,14 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int a, b, count;
 
+	count = 0;
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		for (b = 0; accept[b] != '\0'; b++)
 		{
+			if (count == s[a])
+				break;
+
 			if (accept[b] == s[count])
 				count++;
 		}
