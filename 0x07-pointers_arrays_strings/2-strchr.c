110d4abbb1639dd4227cdbdf9bf80d56d3eeb623
Felipe <jfpc11@misena.edu.co>
2021-02-22 20:04:25 +0000

    strch

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,21 @@
-test 23
+#include "holberton.h"
+
+/**
+ *_memcpy - fill memory with a constat byte
+ *@dest: destinition of the data memory to be copied
+ *@src: source of the memory
+ *@n: copies the first bytes
+ *
+ *Return: a pointer with the value
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (0);
+}
