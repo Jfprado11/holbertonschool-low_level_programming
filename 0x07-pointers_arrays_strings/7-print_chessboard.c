5c304b122ae68a663bdbdba3d782d83e9b0c3a93
Felipe <jfpc11@misena.edu.co>
2021-02-23 02:09:07 +0000

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- *_strpbrk - function locates a string on any byte
- *@s: string to be compare
- *@accept: string to be compare
+ *print_chessboard - function locates a string on any byte
+ *@a: string with information
  *
  *Return: a pointer with the value
  */
 void print_chessboard(char (*a)[8])
 {
-	int r,l;
+	int r, l;
 
-	for (r = 0;r < 8; r++)
+	for (r = 0; r < 8; r++)
 	{
 		for (l = 0; l < 8; l++)
 		{
