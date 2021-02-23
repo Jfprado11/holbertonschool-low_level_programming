cee7be6db7da4a6fd415f48f1275a871bf9d2193
Felipe <jfpc11@misena.edu.co>
2021-02-23 04:17:23 +0000

    set string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *set_string - function that points to another
+ *@s: pointer from
+ *@to: pointer to 
+ *
+ *return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
