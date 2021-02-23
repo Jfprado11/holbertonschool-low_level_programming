8fea53dcfe0dcb713f98f418dc7909a08e88605a
Felipe <jfpc11@misena.edu.co>
2021-02-23 01:26:23 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+int cmp(char *x, char *y);
+/**
+ *_strstr - function locates a substring
+ *@haystack: string to be compare
+ *@needle: string to be compare
+ *
+ *Return: a pointer with the value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle && cmp(haystack, needle))
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
+
+/**
+*cmp - function to compare two strings
+*@x: string to be compare
+*@y: string to be compare
+*
+*Return: a pointer with the value
+*/
+int cmp(char *x, char *y)
+{
+	while (*x && *y)
+	{
+		if (*x != *y)
+		{
+			return (0);
+		}
+		x++;
+		y++;
+	}
+	return (*y == '\0');
+}
