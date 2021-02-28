738692041dd828912a8c5a61c9cf98ed5b90a426
Felipe <jfpc11@misena.edu.co>
2021-02-28 18:46:04 +0000

    without segmentetion fault

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
 
 	for (a = 1; a < argc; a++)
 	{
-		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
 	}
-	if (argc > 1)
+	if (argc > 2)
 	{
+		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
 		printf("%d\n", mul);
 		return (0);
 	}
-	if (argc <= 1)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
