025a80a8f9d93f2ef51848c985bb63fe41e77c32
Felipe <jfpc11@misena.edu.co>
2021-02-28 19:36:06 +0000

    a function that prints

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - programs taht tells its name
+ *@argc: varible counts
+ *@argv: a string
+ *
+ *Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int plus = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	if (argc > 1)
+	{
+		for (a = 1; a < argc; a++)
+		{
+		if (!isdigit(*argv[a]))
+		{
+		printf("Error\n");
+		return (1);
+	}
+		else
+		{
+		plus += atoi(argv[a]);
+	}
+	}
+		printf("%d\n", plus);
+	}
+	return (0);
+}
