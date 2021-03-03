4663a3bda0c80fa50f30a5566486db9baef2a91d
Felipe <jfpc11@misena.edu.co>
2021-03-03 02:08:59 +0000

    separeting the two if

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,13 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	int i = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		return (0);
+	}
+	if (s2 == NULL)
+	{
+		return (0);
 	}
 	cat = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (cat == NULL)
