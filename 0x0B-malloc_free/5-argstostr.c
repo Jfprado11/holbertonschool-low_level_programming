651bc9721c290a9a1928fa635589ccc606726c08
Felipe <jfpc11@misena.edu.co>
2021-03-04 15:55:40 +0000

    contacnates all the arguments of a function

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+int _strlen(char *s);
+/**
+ *argstostr - concatenates all the arguments of your program
+ *@ac: vairble with number of arg
+ *@av: variable with the string.
+ *
+ *Return: a poitner with a string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, l = 0;
+	int len = 0;
+	char *astr;
+
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+	astr = malloc(sizeof(char) * (len + 1));
+
+	for (j = 0; j < ac; j++)
+	{
+		for (k = 0; av[j][k] != '\0'; k++)
+		{
+			astr[l] = av[j][k];
+			l++;
+		}
+		astr[l] = '\n';
+		l++;
+	}
+	astr[l] = '\0';
+	return (astr);
+}
+/**
+ *_strlen - Tells the length or size of a string
+ *@s: varible to test the string
+ *
+ *Return: len the length of the string
+ */
+int _strlen(char *s)
+{
+	int lon = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		lon++;
+	}
+	return (lon);
+}
