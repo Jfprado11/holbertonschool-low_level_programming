d0b9a51d6c45a1745d32c6a59b079013a6ec1535
Felipe <jfpc11@misena.edu.co>
2021-03-08 22:04:40 +0000

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+
+
+/**
+ * struct dog - Some values
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
+
+#endif
