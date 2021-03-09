09d66728f4363ecd098abc10c486c2d9e74edabb
Felipe <jfpc11@misena.edu.co>
2021-03-09 03:44:26 +0000

    with a zero

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,13 +9,13 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d != '\0')
+	if (d != 0)
 	{
-		if (d->name != '\0')
+		if (d->name != 0)
 		{
 			free(d->name);
 		}
-		if (d->owner != '\0')
+		if (d->owner != 0)
 		{
 			free(d->owner);
 		}
