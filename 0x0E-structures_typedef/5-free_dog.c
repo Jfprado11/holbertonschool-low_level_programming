cba67ef736ca2875458f15c88d582417cf42f5de
Felipe <jfpc11@misena.edu.co>
2021-03-09 03:41:12 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *free_dog -  function that frees dogs
+ *@d: structure
+ *
+ *Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != '\0')
+	{
+		if (d->name != '\0')
+		{
+			free(d->name);
+		}
+		if (d->owner != '\0')
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
