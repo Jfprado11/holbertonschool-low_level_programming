a8d55ca179103d43a8e4d5044b29275063597497
Felipe <jfpc11@misena.edu.co>
2021-03-11 23:16:46 +0000

    a function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - function that prints the int numbers
+ *@separator: string going t=in the middle of num
+ *@n: numebers of ag
+ *
+ *Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a, b;
+	va_list ag;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	va_start(ag, n);
+	for (a = 0; a < n; a++)
+	{
+		b = va_arg(ag, int);
+		if (a == n - 1)
+		{
+			printf("%d", b);
+		}
+		else
+		{
+			printf("%d%s", b, separator);
+		}
+	}
+	printf("\n");
+	va_end(ag);
+}
