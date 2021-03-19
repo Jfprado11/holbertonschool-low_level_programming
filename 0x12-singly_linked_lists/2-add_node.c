a4e330d2f6313d98cf9568d979afbaa49bc53c71
Felipe <jfpc11@misena.edu.co>
2021-03-19 13:40:47 +0000

    add a node at beging of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <string.h>
+
+int _strlen(char *s);
+/**
+ *add_node - adds a node at begging of the list
+ *@head: the head of list
+ *@str: string to be duplicated
+ *
+ *Return: the addreos the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *beg_node = NULL;
+
+	beg_node = malloc(sizeof(list_t));
+	if (beg_node == NULL)
+	{
+		return (NULL);
+	}
+	beg_node->str = strdup(str);
+	beg_node->len = _strlen(beg_node->str);
+	beg_node->next = (*head);
+	(*head) = beg_node;
+	return (beg_node);
+}
+
+/**
+ *_strlen - Tells the length or size of a string
+ *@s: varible to test the string
+ *
+ *Return: len the length of the string
+ */
+int _strlen(char *s)
+{
+	int lon = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		lon++;
+	}
+	return (lon);
+}
