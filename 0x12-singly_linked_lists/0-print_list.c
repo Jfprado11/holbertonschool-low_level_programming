52c230f26606545ac73763fa6719685f25ba8891
Felipe <jfpc11@misena.edu.co>
2021-03-19 02:57:19 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ *print_list - print all the lements of a linked list.
+ *@h: the head of the list
+ *
+ *Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	const list_t *ptr = NULL;
+	const list_t *n = NULL;
+
+	n = h;
+	ptr = h;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+	}
+	while(n != NULL)
+	{
+		count++;
+		n = n->next;
+	}
+	return (count);
+}
