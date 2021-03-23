49ef1df7e84959e25a87ab6ab6a395ff63205884
Felipe <jfpc11@misena.edu.co>
2021-03-23 17:21:20 +0000

    a function to sum all the data inside a linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *sum_listint - sum all the data inside of the linked list
+ *@head: the head of linked list
+ *
+ *Return: the sum of the data
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+	int sum;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (ptr != NULL)
+	{
+		sum = sum + ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
