18902fc40cbd860f985962a43485da9e71f4565b
Felipe <jfpc11@misena.edu.co>
2021-03-24 01:03:40 +0000

    delete a node in certain point

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ *delete_nodeint_at_index - delete a node at certain point
+ *@head: the head of the lists
+ *@index: the idex of the node
+ *Return: 1 in success. -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *previo = *head;
+	listint_t *ptr = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		*head = ptr->next;
+		free(ptr);
+		ptr = NULL;
+		return (1);
+	}
+	else
+	{
+		while (index != 0)
+		{
+			previo = ptr;
+			ptr = ptr->next;
+			index--;
+		}
+		previo->next = ptr->next;
+		free(ptr);
+		ptr = NULL;
+		return (1);
+	}
+}
