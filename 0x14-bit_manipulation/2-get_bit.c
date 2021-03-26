048714c3deba4491679ad6b19444bdbfca294a26
Felipe <jfpc11@misena.edu.co>
2021-03-26 03:23:04 +0000

    adding a if

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,6 +14,10 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	result = n >> index;
 	result = result & 1;
+	if (result == NULL)
+	{
+		return (-1);
+	}
 	if (result == 1)
 	{
 		return (1);
