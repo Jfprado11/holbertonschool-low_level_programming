86e58c2e872fe4271ee746d9c359cfc87f9b8055
Felipe Prado <jfpc11@misena.edu.co>
2021-03-29 14:26:07 -0500

    function that reads a file and then prints it.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *read_textfile - reads a files and prints
+ *@filename: the file to be redeable
+ *@letters: the amount of letters to be printed and read
+ *
+ *Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buf[letters];
+	ssize_t count1, count2;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	count1 = read(fd, buf, letters);
+	if (count1 == -1)
+	{
+		return (0);
+	}
+	count2 = write(STDOUT_FILENO, buf, count1);
+	if (count2 == -1)
+	{
+		return (-1);
+	}
+	return (count2);
+}
