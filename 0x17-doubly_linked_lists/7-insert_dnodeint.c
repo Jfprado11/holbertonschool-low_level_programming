03c8d145fd8ae5abc07aa98e99ce26f55b56b91a
Felipe Prado <jfpc11@misena.edu.co>
2021-05-01 12:27:32 -0500

    checking the last node: : :

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,10 +33,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			aux = aux->next;
 			i++;
 		}
-		if (aux->next == NULL)
-		{
-			return (add_dnodeint_end(&*h, n));
-		}
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
 			return (NULL);
