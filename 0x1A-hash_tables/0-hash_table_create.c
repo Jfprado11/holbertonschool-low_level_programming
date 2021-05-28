bc464e81f176a395c3f4919d5fe7b51c3a9f5a8e
Felipe Prado <jfpc11@misena.edu.co>
2021-05-28 11:19:56 -0500

    function to create a hash table without calloc

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_create - create a hash table
+ *@size: the size of the table
+ *
+ *Return: a pointer to creted table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof (hash_table_t *) * table->size);
+	if (table->array == NULL)
+		return NULL;
+
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
