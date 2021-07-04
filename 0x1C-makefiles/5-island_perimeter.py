e40d5de81af5e915211f7c69f2d75d64b3ec80a8
Felipe Prado <jfpc11@misena.edu.co>
2021-07-03 20:51:56 -0500

    adding an algorithm that checks if a number is correct

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    count = 0
+    lists = len(grid)
+    single = len(grid[0])
+    for y in range(lists):
+        for x in range(single):
+            if grid[y][x] == 1:
+                if grid[y - 1][x] == 0:
+                    count += 1
+                if grid[y][x - 1] == 0:
+                    count += 1
+                if grid[y][x + 1] == 0:
+                    count += 1
+                if grid[y + 1][x] == 0:
+                    count += 1
+
+    return count
