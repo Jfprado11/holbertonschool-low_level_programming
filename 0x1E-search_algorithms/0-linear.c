04c4cefc5da1d6b16d47af52a27d9d0f4f2373a2
Felipe Prado <jfpc11@misena.edu.co>
2021-08-30 17:32:28 -0500

    adding the function to produce the liniar search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - perform a liniar search
+ * @array: a pointer to to first element of teh array
+ * @size: is the number of elements in the array
+ * @value: the value to search for
+ *
+ * Return:
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
