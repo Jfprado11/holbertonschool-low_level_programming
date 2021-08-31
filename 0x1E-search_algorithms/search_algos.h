66c21ffbeb63faf1209fcdbd524d9f49ab319343
Felipe Prado <jfpc11@misena.edu.co>
2021-08-31 10:58:41 -0500

    adding a prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,6 @@
 /*prototype*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*__SEARCH_ALGO_H_*/
