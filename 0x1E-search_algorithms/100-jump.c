e6cb9696a851643362fdea0197b1591f19ba96ae
Felipe Prado <jfpc11@misena.edu.co>
2021-08-31 12:02:01 -0500

    changing the output

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
 #include <math.h>
 
+size_t _min(size_t left, size_t rigth);
 /**
  *jump_search - searches for a value in array of integers using the Jump search
  *@array: the array to be search
@@ -12,26 +13,52 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step, x, i = 0;
+	size_t step, i = 0;
 
 	if (array == NULL)
 		return (-1);
 	step = sqrt(size);
-	while (array[step] <= value && step < size)
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	while (array[_min(step, size)] < value && step < size)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		i = step;
 		step += sqrt(size);
-		if (step > size - 1)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (i >= size)
 			return (-1);
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n", i, step);
-	for (x = i; x < step; x++)
+	while (array[i] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
-		if (array[x] == value)
-			return (x);
+		if (i == _min(step, size))
+			return (-1);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i++;
 	}
-	return (-1);
+	if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+		}
+	return(-1);
+}
+
+/**
+ * _min - return the less value
+ * @left: a value to be compared
+ * @rigth: a value to be compared
+ *
+ * Return: the less value
+ */
+size_t _min(size_t left, size_t rigth)
+{
+	if (left < rigth)
+	{
+		return (left);
+	}
+	else
+	{
+		return (rigth);
+	}
 }
