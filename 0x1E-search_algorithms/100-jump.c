64da0367f09e6e8d8297ffeb96d7c75c9319d008
Felipe Prado <jfpc11@misena.edu.co>
2021-08-31 12:25:40 -0500

    please

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -19,7 +19,7 @@ int jump_search(int *array, size_t size, int value)
 		return (-1);
 	step = sqrt(size);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	while (array[_min(step, size)] < value && step < size)
+	while (array[_min(step, size)] < value)
 	{
 		i = step;
 		step += sqrt(size);
